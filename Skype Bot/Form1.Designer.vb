e92e0afcd464429b3130c2d9d5e516ca987dd550
WelpeLP <welpe93@gmail.com>
2016-07-18 16:49:29 +0200

    Uploaded WelpeBot

---
+++ Skype Bot/Form1.Designer.vb
@@ -0,0 +1,288 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TabControl = New System.Windows.Forms.TabControl()
+        Me.TabSettings = New System.Windows.Forms.TabPage()
+        Me.btnwelmeschange = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtWelcome = New System.Windows.Forms.TextBox()
+        Me.btnopenurl = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtUrl = New System.Windows.Forms.TextBox()
+        Me.btnmotdchange = New System.Windows.Forms.Button()
+        Me.btnprechange = New System.Windows.Forms.Button()
+        Me.txtPrefix = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtMotd = New System.Windows.Forms.TextBox()
+        Me.TabMusicNow = New System.Windows.Forms.TabPage()
+        Me.NowPlayingWeb = New System.Windows.Forms.WebBrowser()
+        Me.TabMusicQueue = New System.Windows.Forms.TabPage()
+        Me.MusicQueueTxt = New System.Windows.Forms.RichTextBox()
+        Me.TabAbout = New System.Windows.Forms.TabPage()
+        Me.TabControl.SuspendLayout()
+        Me.TabSettings.SuspendLayout()
+        Me.TabMusicNow.SuspendLayout()
+        Me.TabMusicQueue.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabControl
+        '
+        Me.TabControl.Controls.Add(Me.TabSettings)
+        Me.TabControl.Controls.Add(Me.TabMusicNow)
+        Me.TabControl.Controls.Add(Me.TabMusicQueue)
+        Me.TabControl.Controls.Add(Me.TabAbout)
+        Me.TabControl.Location = New System.Drawing.Point(-3, 0)
+        Me.TabControl.Name = "TabControl"
+        Me.TabControl.SelectedIndex = 0
+        Me.TabControl.Size = New System.Drawing.Size(323, 413)
+        Me.TabControl.TabIndex = 0
+        '
+        'TabSettings
+        '
+        Me.TabSettings.BackColor = System.Drawing.SystemColors.Highlight
+        Me.TabSettings.Controls.Add(Me.btnwelmeschange)
+        Me.TabSettings.Controls.Add(Me.Label4)
+        Me.TabSettings.Controls.Add(Me.txtWelcome)
+        Me.TabSettings.Controls.Add(Me.btnopenurl)
+        Me.TabSettings.Controls.Add(Me.Label3)
+        Me.TabSettings.Controls.Add(Me.txtUrl)
+        Me.TabSettings.Controls.Add(Me.btnmotdchange)
+        Me.TabSettings.Controls.Add(Me.btnprechange)
+        Me.TabSettings.Controls.Add(Me.txtPrefix)
+        Me.TabSettings.Controls.Add(Me.Label2)
+        Me.TabSettings.Controls.Add(Me.Label1)
+        Me.TabSettings.Controls.Add(Me.txtMotd)
+        Me.TabSettings.Location = New System.Drawing.Point(4, 24)
+        Me.TabSettings.Name = "TabSettings"
+        Me.TabSettings.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabSettings.Size = New System.Drawing.Size(315, 385)
+        Me.TabSettings.TabIndex = 0
+        Me.TabSettings.Text = "Settings"
+        '
+        'btnwelmeschange
+        '
+        Me.btnwelmeschange.Location = New System.Drawing.Point(194, 332)
+        Me.btnwelmeschange.Name = "btnwelmeschange"
+        Me.btnwelmeschange.Size = New System.Drawing.Size(109, 39)
+        Me.btnwelmeschange.TabIndex = 24
+        Me.btnwelmeschange.Text = "Change Welcome Message"
+        Me.btnwelmeschange.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(8, 290)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(98, 13)
+        Me.Label4.TabIndex = 23
+        Me.Label4.Text = "Welcome Message"
+        '
+        'txtWelcome
+        '
+        Me.txtWelcome.AcceptsReturn = True
+        Me.txtWelcome.Location = New System.Drawing.Point(8, 306)
+        Me.txtWelcome.Multiline = True
+        Me.txtWelcome.Name = "txtWelcome"
+        Me.txtWelcome.Size = New System.Drawing.Size(301, 20)
+        Me.txtWelcome.TabIndex = 22
+        Me.txtWelcome.Text = "Welcome to WelpeBot. This is a BOT so there will not be any human replies. For a " &
+    "list of commands type !commands."
+        '
+        'btnopenurl
+        '
+        Me.btnopenurl.Location = New System.Drawing.Point(194, 240)
+        Me.btnopenurl.Name = "btnopenurl"
+        Me.btnopenurl.Size = New System.Drawing.Size(109, 39)
+        Me.btnopenurl.TabIndex = 21
+        Me.btnopenurl.Text = "Open URL"
+        Me.btnopenurl.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(8, 198)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(89, 13)
+        Me.Label3.TabIndex = 20
+        Me.Label3.Text = "Open Music URL"
+        '
+        'txtUrl
+        '
+        Me.txtUrl.AcceptsReturn = True
+        Me.txtUrl.Location = New System.Drawing.Point(8, 214)
+        Me.txtUrl.Multiline = True
+        Me.txtUrl.Name = "txtUrl"
+        Me.txtUrl.Size = New System.Drawing.Size(301, 20)
+        Me.txtUrl.TabIndex = 19
+        Me.txtUrl.Text = "http://example.com"
+        '
+        'btnmotdchange
+        '
+        Me.btnmotdchange.Location = New System.Drawing.Point(194, 145)
+        Me.btnmotdchange.Name = "btnmotdchange"
+        Me.btnmotdchange.Size = New System.Drawing.Size(109, 41)
+        Me.btnmotdchange.TabIndex = 18
+        Me.btnmotdchange.Text = "Change MOTD"
+        Me.btnmotdchange.UseVisualStyleBackColor = True
+        '
+        'btnprechange
+        '
+        Me.btnprechange.Location = New System.Drawing.Point(194, 59)
+        Me.btnprechange.Name = "btnprechange"
+        Me.btnprechange.Size = New System.Drawing.Size(109, 38)
+        Me.btnprechange.TabIndex = 17
+        Me.btnprechange.Text = "Change Prefix"
+        Me.btnprechange.UseVisualStyleBackColor = True
+        '
+        'txtPrefix
+        '
+        Me.txtPrefix.AcceptsReturn = True
+        Me.txtPrefix.Location = New System.Drawing.Point(8, 33)
+        Me.txtPrefix.Multiline = True
+        Me.txtPrefix.Name = "txtPrefix"
+        Me.txtPrefix.Size = New System.Drawing.Size(301, 20)
+        Me.txtPrefix.TabIndex = 16
+        Me.txtPrefix.Text = "!"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(8, 17)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(83, 13)
+        Me.Label2.TabIndex = 15
+        Me.Label2.Text = "Command Prefix"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(8, 103)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(100, 13)
+        Me.Label1.TabIndex = 14
+        Me.Label1.Text = "Message of the day"
+        '
+        'txtMotd
+        '
+        Me.txtMotd.AcceptsReturn = True
+        Me.txtMotd.Location = New System.Drawing.Point(8, 119)
+        Me.txtMotd.Multiline = True
+        Me.txtMotd.Name = "txtMotd"
+        Me.txtMotd.Size = New System.Drawing.Size(301, 20)
+        Me.txtMotd.TabIndex = 13
+        Me.txtMotd.Text = "This is the default MOTD message."
+        '
+        'TabMusicNow
+        '
+        Me.TabMusicNow.Controls.Add(Me.NowPlayingWeb)
+        Me.TabMusicNow.Location = New System.Drawing.Point(4, 24)
+        Me.TabMusicNow.Name = "TabMusicNow"
+        Me.TabMusicNow.Size = New System.Drawing.Size(315, 385)
+        Me.TabMusicNow.TabIndex = 3
+        Me.TabMusicNow.Text = "Now playing"
+        Me.TabMusicNow.UseVisualStyleBackColor = True
+        '
+        'NowPlayingWeb
+        '
+        Me.NowPlayingWeb.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.NowPlayingWeb.Location = New System.Drawing.Point(0, 0)
+        Me.NowPlayingWeb.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.NowPlayingWeb.Name = "NowPlayingWeb"
+        Me.NowPlayingWeb.Size = New System.Drawing.Size(315, 385)
+        Me.NowPlayingWeb.TabIndex = 0
+        Me.NowPlayingWeb.Url = New System.Uri("http://youtube.com", System.UriKind.Absolute)
+        '
+        'TabMusicQueue
+        '
+        Me.TabMusicQueue.BackColor = System.Drawing.SystemColors.Control
+        Me.TabMusicQueue.Controls.Add(Me.MusicQueueTxt)
+        Me.TabMusicQueue.Location = New System.Drawing.Point(4, 24)
+        Me.TabMusicQueue.Name = "TabMusicQueue"
+        Me.TabMusicQueue.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabMusicQueue.Size = New System.Drawing.Size(315, 385)
+        Me.TabMusicQueue.TabIndex = 1
+        Me.TabMusicQueue.Text = "Music Queue"
+        '
+        'MusicQueueTxt
+        '
+        Me.MusicQueueTxt.Location = New System.Drawing.Point(6, 6)
+        Me.MusicQueueTxt.Name = "MusicQueueTxt"
+        Me.MusicQueueTxt.ReadOnly = True
+        Me.MusicQueueTxt.Size = New System.Drawing.Size(303, 373)
+        Me.MusicQueueTxt.TabIndex = 0
+        Me.MusicQueueTxt.Text = ""
+        '
+        'TabAbout
+        '
+        Me.TabAbout.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.TabAbout.Location = New System.Drawing.Point(4, 24)
+        Me.TabAbout.Name = "TabAbout"
+        Me.TabAbout.Size = New System.Drawing.Size(315, 385)
+        Me.TabAbout.TabIndex = 2
+        Me.TabAbout.Text = "About"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(316, 410)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.TabControl)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "WelpeBot for Skype"
+        Me.TabControl.ResumeLayout(False)
+        Me.TabSettings.ResumeLayout(False)
+        Me.TabSettings.PerformLayout()
+        Me.TabMusicNow.ResumeLayout(False)
+        Me.TabMusicQueue.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TabControl As TabControl
+    Friend WithEvents TabSettings As TabPage
+    Friend WithEvents btnwelmeschange As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtWelcome As TextBox
+    Friend WithEvents btnopenurl As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtUrl As TextBox
+    Friend WithEvents btnmotdchange As Button
+    Friend WithEvents btnprechange As Button
+    Friend WithEvents txtPrefix As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtMotd As TextBox
+    Friend WithEvents TabMusicQueue As TabPage
+    Friend WithEvents TabAbout As TabPage
+    Friend WithEvents MusicQueueTxt As RichTextBox
+    Friend WithEvents TabMusicNow As TabPage
+    Public WithEvents NowPlayingWeb As WebBrowser
+End Class
---
+++ Skype Bot/Form1.vb
@@ -0,0 +1,96 @@
+﻿Imports SKYPE4COMLib
+
+Public Class Form1
+    Private skype As Skype
+    Public trigger As String = "!" 'This is the trigger mark
+    Public motd As String = "DefaultString"
+    Public url As String = "http://example.com"
+    Public chkFR As Object
+    Public welcome As Object = "Welcome to WelpeBot. This is a BOT so there will not be any human replies. For a list of commands type !commands."
+    Public chatUrl As String
+
+    'This is the URL Queue for music playback
+    Public nowplaying As String
+    Public queue1 As String
+    Public queue2 As String
+    Public queue3 As String
+    Public queue4 As String
+    Public queue5 As String
+    Public queue6 As String
+    Public queue7 As String
+    Public queue8 As String
+    Public queue9 As String
+    Public queue10 As String
+    Public queue11 As String
+    Public queue12 As String
+    Public queue13 As String
+    Public queue14 As String
+    Public queue15 As String
+    Public queue16 As String
+    Public queue17 As String
+    Public queue18 As String
+    Public queue19 As String
+    Public queue20 As String
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        skype = New Skype
+        skype.Attach(7, False) 'Attaches the program to Skype. Will need to be allowed through Skype itself. 
+        AddHandler skype.MessageStatus, AddressOf skype_stat
+    End Sub
+    Public Sub skype_stat(ByVal msg As ChatMessage, ByVal status As TChatMessageStatus)
+        If msg.Body.Contains(trigger) Then '! Is the trigger here. If the ! is detected then the trigger will be deteced
+            Dim sendme As String = msg.Body.Replace(trigger, "") 'removes the trigger from the message. for example if the message was "!hello", the it would be "hello"
+            skype.SendMessage(msg.Sender.Handle, cases(sendme)) 'sends message.
+        ElseIf msg.Body.Contains("http") Then
+
+            NowPlayingWeb.Navigate("http://www.google.com")
+        End If
+
+    End Sub
+    Public Function cases(ByVal sent As String)
+        Dim result As String
+        Select Case sent
+            Case Is = "info" 'What the user will need to type. 
+                result = "]|[ WELPEBOT ]|[" + vbNewLine + "Welcome to WelpeBot. This is a BOT so there will not be any human replies. For a list of commands type !commands." 'What the bot will reply with
+                Exit Select
+            Case Is = "help"
+                result = "]|[ WELPEBOT ]|[" + vbNewLine + "!info - Will display the help message" + vbNewLine + "!help - Will display all the commands you can use with this bot." + vbNewLine + "!motd - Will Show the active Message Of The Day" + vbNewLine + "!playmusic - Will Play 'Sia - Cheap Thrills'." + vbNewLine + "!stopmusic - Will Stop the Audio Playback."
+                Exit Select
+            Case Is = "motd"
+                result = "]|[ WELPEBOT ]|[" + vbNewLine + "}{|}---MOTD---{|}{" + vbNewLine + txtMotd.Text
+                Exit Select
+            Case Is = "playmusic"
+                result = "]|[ WELPEBOT ]|[" + vbNewLine + "Playing song For you..."
+
+                Exit Select
+            Case Else
+                result = "]|[ WELPEBOT ]|[" + vbNewLine + "I don't understand what you typed"
+        End Select
+        Return result
+    End Function
+
+    Private Sub btnprechange_Click(sender As Object, e As EventArgs)
+        trigger = txtPrefix.Text
+    End Sub
+
+    Private Sub btnmotdchange_Click(sender As Object, e As EventArgs)
+        motd = txtMotd.Text
+    End Sub
+
+    Private Sub btnopenurl_Click(sender As Object, e As EventArgs)
+        url = txtUrl.Text
+        Process.Start(url)
+    End Sub
+
+    Private Sub btnwelmeschange_Click(sender As Object, e As EventArgs) Handles btnopenurl.Click
+        welcome = txtWelcome.Text
+    End Sub
+
+    Private Sub ISkype_unauthorizedrequestreceived(User As User)
+        If chkFR.Checked = True And User.IsAuthorized = False Then
+            User.IsAuthorized = True
+            welcome = txtWelcome.Text
+
+        End If
+    End Sub
+End Class
