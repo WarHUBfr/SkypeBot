46ffcaec28d82be721e7bc56de0bf5046e6ed17c
William Johnstone <williamj04@outlook.com>
2016-07-22 13:22:45 +0100

    Uploaded Files

---
+++ ZokiBot/Form1.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtMotd = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtPrefix = New System.Windows.Forms.TextBox()
+        Me.btnprechange = New System.Windows.Forms.Button()
+        Me.btnmotdchange = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtMotd
+        '
+        Me.txtMotd.AcceptsReturn = True
+        Me.txtMotd.Location = New System.Drawing.Point(12, 23)
+        Me.txtMotd.Multiline = True
+        Me.txtMotd.Name = "txtMotd"
+        Me.txtMotd.Size = New System.Drawing.Size(260, 20)
+        Me.txtMotd.TabIndex = 0
+        Me.txtMotd.Text = "This is the default MOTD message."
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(89, 7)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(100, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Message of the day"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(89, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(83, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Command Prefix"
+        '
+        'txtPrefix
+        '
+        Me.txtPrefix.AcceptsReturn = True
+        Me.txtPrefix.Location = New System.Drawing.Point(12, 74)
+        Me.txtPrefix.Multiline = True
+        Me.txtPrefix.Name = "txtPrefix"
+        Me.txtPrefix.Size = New System.Drawing.Size(260, 20)
+        Me.txtPrefix.TabIndex = 4
+        Me.txtPrefix.Text = "!"
+        '
+        'btnprechange
+        '
+        Me.btnprechange.Location = New System.Drawing.Point(278, 72)
+        Me.btnprechange.Name = "btnprechange"
+        Me.btnprechange.Size = New System.Drawing.Size(109, 23)
+        Me.btnprechange.TabIndex = 5
+        Me.btnprechange.Text = "Change Prefix"
+        Me.btnprechange.UseVisualStyleBackColor = True
+        '
+        'btnmotdchange
+        '
+        Me.btnmotdchange.Location = New System.Drawing.Point(278, 23)
+        Me.btnmotdchange.Name = "btnmotdchange"
+        Me.btnmotdchange.Size = New System.Drawing.Size(109, 23)
+        Me.btnmotdchange.TabIndex = 6
+        Me.btnmotdchange.Text = "Change MOTD"
+        Me.btnmotdchange.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(401, 110)
+        Me.Controls.Add(Me.btnmotdchange)
+        Me.Controls.Add(Me.btnprechange)
+        Me.Controls.Add(Me.txtPrefix)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtMotd)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Zoki Skype Bot"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtMotd As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtPrefix As TextBox
+    Friend WithEvents btnprechange As Button
+    Friend WithEvents btnmotdchange As Button
+End Class
---
+++ ZokiBot/Form1.vb
@@ -0,0 +1,45 @@
+﻿Imports SKYPE4COMLib
+
+Public Class Form1
+    Private skype As Skype
+    Public trigger As String = "!" 'This is the trigger mark
+    Public motd As String = "Default"
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        skype = New Skype
+        skype.Attach() 'Attaches the program to Skype. Will need to be allowed through Skype itself. 
+        AddHandler skype.MessageStatus, AddressOf skype_stat
+    End Sub
+    Public Sub skype_stat(ByVal msg As ChatMessage, ByVal status As TChatMessageStatus)
+        If msg.Body.Contains(trigger) Then '! Is the trigger here. If the ! is detected then the trigger will be deteced
+            Dim sendme As String = msg.Body.Replace(trigger, "") 'removes the trigger from the message. for example if the message was "!hello", the it would be "hello"
+            skype.SendMessage(msg.Sender.Handle, cases(sendme)) 'sends message.
+        End If
+
+    End Sub
+    Public Function cases(ByVal sent As String)
+        Dim result As String
+        Select Case sent
+            Case Is = "help" 'What the user will need to type. 
+                result = "[- Zoki Bot -]" + vbNewLine + "Welcome to Zoki Bot. This is a BOT so there will not be any human replies. For a list of commands type !commands." 'What the bot will reply with
+                Exit Select
+            Case Is = "commands"
+                result = "[- Zoki Bot -]" + vbNewLine + "!help - Will display the help message" + vbNewLine + "!commands - Will display all the commands you can use with this bot." + vbNewLine + "!motd - Will Show the active Message Of The Day"
+                Exit Select
+            Case Is = "motd"
+                result = "[- Zoki Bot -]" + vbNewLine + "[---Message Of The Day---]" + vbNewLine + motd
+                Exit Select
+            Case Else
+                result = "[- Zoki Bot -]" + vbNewLine + "Not a valid command"
+        End Select
+        Return result
+    End Function
+
+    Private Sub btnprechange_Click(sender As Object, e As EventArgs) Handles btnprechange.Click
+        trigger = txtPrefix.Text
+    End Sub
+
+    Private Sub btnmotdchange_Click(sender As Object, e As EventArgs) Handles btnmotdchange.Click
+        motd = txtMotd.Text
+    End Sub
+End Class
